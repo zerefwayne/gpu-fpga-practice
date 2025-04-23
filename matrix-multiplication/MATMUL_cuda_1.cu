604b25afbc937d427ffcaff60da53d78dc56f40b
Aayush Joglekar <aayushjog@gmail.com>
2025-04-23 13:10:12 +0200

    Init cuda_1 file and make dynamic make commands

---
+++ matrix-multiplication/MATMUL_cuda_1.cu
@@ -0,0 +1,12 @@
+// CUDA Implementation for Matrix Matrix Multiplication
+// Version 1: No optimizations, just a basic port
+
+#include "MATMUL.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+extern "C" void do_compute(struct parameters *p) {
+    
+}
--- matrix-multiplication/job.sh
+++ matrix-multiplication/job.sh
@@ -4,17 +4,40 @@
 #SBATCH --gres=gpu:1                  # Request 1 GPU resource
 #SBATCH --constraint=TitanX           # Specify a hardware constraint (e.g., TitanX GPU)
 
+# Validate input arguments
 
-# ./bin/MATMUL_seq ./input/input_2_2_2_2.in
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <binary_file_path> <problem_size (S/M/L)>"
+    exit 1
+fi
 
-for i in {10..100..10}; do
-    ./bin/MATMUL_seq ./input/input_${i}_${i}_${i}_${i}.in
-done
+if [ ! -x "$1" ]; then
+    echo "Error: The binary file '$1' does not exist or is not executable."
+    exit 1
+fi
 
-for i in {200..1000..100}; do
-    ./bin/MATMUL_seq ./input/input_${i}_${i}_${i}_${i}.in
-done
+if [ "$2" != "S" ] && [ "$2" != "M" ] && [ "$2" != "L" ]; then
+    echo "Error: Problem size must be one of 'S', 'M', or 'L'."
+    exit 1
+fi
 
-# for i in {2000..4000..1000}; do
-#     ./bin/MATMUL_seq ./input/input_${i}_${i}_${i}_${i}.in
-# done
+
+# Run binaries for different sizes
+
+if [ "$2" = "S" ] || [ "$2" = "M" ] || [ "$2" = "L" ]; then
+    for i in {10..100..10}; do
+        "$1" ./input/input_${i}_${i}_${i}_${i}.in
+    done
+fi
+
+if [ "$2" = "M" ] || [ "$2" = "L" ]; then
+    for i in {200..1000..100}; do
+        "$1" ./input/input_${i}_${i}_${i}_${i}.in
+    done
+fi
+
+if [ "$2" = "L" ]; then
+    for i in {2000..4000..1000}; do
+        ./bin/MATMUL_seq ./input/input_${i}_${i}_${i}_${i}.in
+    done
+fi
